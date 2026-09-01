e4616dede07cf3ea281e5847c51028ebf40a2720
Cheng-Chen, Chen <chen21019@gmail.com>
2026-09-01 11:18:25 +0800

    release: prepare server v1.6.396 (#55)

--- server/build-api-explorer-patch-image.sh
+++ server/build-api-explorer-patch-image.sh
@@ -27,13 +27,13 @@ api_explorer_artifact=${API_EXPLORER_ARTIFACT:-api-explorer-1.1.18.tar.gz}
 api_explorer_artifact_sha256=${API_EXPLORER_ARTIFACT_SHA256:-92b718c46163018ea40c008ac552911f0eb610647377725405f4046dcd411f2c}
 api_explorer_commit=${API_EXPLORER_COMMIT:-3b1c39e8a116f58649d94233a384a0362c02b43e}
 web_console_release_base_url=${WEB_CONSOLE_RELEASE_BASE_URL:-https://github.com/PastureStack/web-console/releases/download}
-web_console_release_tag=${WEB_CONSOLE_RELEASE_TAG:-1.6.97}
-web_console_artifact=${WEB_CONSOLE_ARTIFACT:-web-console-1.6.97.tar.gz}
-web_console_artifact_sha256=${WEB_CONSOLE_ARTIFACT_SHA256:-d033bfb0a5f361b2ed97774c460aba29b9e91ce7a81a8062fbe686249f39e0b8}
-web_console_commit=${WEB_CONSOLE_COMMIT:-07ca6f3a32276f66abd1e5bfe8f74600bffc2b9c}
+web_console_release_tag=${WEB_CONSOLE_RELEASE_TAG:-1.6.98}
+web_console_artifact=${WEB_CONSOLE_ARTIFACT:-web-console-1.6.98.tar.gz}
+web_console_artifact_sha256=${WEB_CONSOLE_ARTIFACT_SHA256:-2e7a34ca1c39cc0f0425bf8a0165496510149e4ef6f9e7938d8871edb96edbff}
+web_console_commit=${WEB_CONSOLE_COMMIT:-ee871276309fbc95af85f369eaf2c22174490fbc}
 supported_docker_range='~v1.12.3 || ~v1.13.0 || ~v17.03.0 || ~v17.06.0 || ~v17.09.0 || ~v17.12.0 || ~v18.03.0 || ~v18.06.0 || ~v18.09.0 || ~v19.03.2 || v24.0.9 || >=v29.4.1 <=v29.7.2'
 newest_docker_version=v29.7.2
-image=${IMAGE:-pasturestack-validation/server:v1.6.395}
+image=${IMAGE:-pasturestack-validation/server:v1.6.396}
 build_options=()
 
 [[ "$revision" =~ ^[0-9a-f]{40}$ ]]
@@ -98,7 +98,7 @@ docker buildx build \
 
 test "$(docker image inspect "$image" \
     --format '{{index .Config.Labels "org.opencontainers.image.version"}}')" = \
-    v1.6.395
+    v1.6.396
 test "$(docker image inspect "$image" \
     --format '{{index .Config.Labels "org.opencontainers.image.revision"}}')" = \
     "$revision"
@@ -109,7 +109,7 @@ test "$(docker image inspect "$image" \
 image_environment=$(docker image inspect "$image" \
     --format '{{range .Config.Env}}{{println .}}{{end}}')
 for marker in \
-    CATTLE_RANCHER_SERVER_VERSION=v1.6.395 \
+    CATTLE_RANCHER_SERVER_VERSION=v1.6.396 \
     CATTLE_API_UI_VERSION=1.1.18 \
     CATTLE_CATTLE_VERSION=v0.183.288 \
     PASTURESTACK_ORCHESTRATION_ENGINE_COMMIT="${orchestration_engine_commit}" \
@@ -204,7 +204,7 @@ docker run --rm --entrypoint bash "$image" -lc 'test -x /usr/bin/websocket-proxy
 docker run --rm --entrypoint bash "$image" -lc '
     set -euo pipefail
     web_root=$(readlink -f /usr/share/cattle/war)
-    test "$(cat "${web_root}/VERSION.txt")" = "1.6.97"
+    test "$(cat "${web_root}/VERSION.txt")" = "1.6.98"
     test "$(find "${web_root}/translations" -maxdepth 1 -type f -name "*.json" | wc -l)" -eq 13
     test ! -e "${web_root}/translations/none.json"
     test -z "$(find "${web_root}" -type f -name "*.map" -print -quit)"
